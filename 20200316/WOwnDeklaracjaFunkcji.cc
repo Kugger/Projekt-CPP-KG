cccf6f85d3f05aaa652689633034b5d8a6904bf1
Kacper Gorgoń <61627168+Kugger@users.noreply.github.com>
2020-03-16 10:34:59 +0100

    Add files via upload

---
+++ 20200316/WOwnDeklaracjaFunkcji.cc
@@ -0,0 +1,23 @@
+/**
+  * \author: Kacper Gorgoń
+  * \date: 2020.03.16
+  * \brief: Stara metoda tworzenia funkcji (nie działa w nowym kompilatorze)
+  * v0.01
+*/
+
+#include <iostream>
+using namespace std;
+
+int Function(a, b, c)
+int a, b, c;
+{
+	cout << "a" << endl;
+}
+
+
+int main() {
+
+	cout << "Test" << endl;
+	Function(1, 2, 3);
+	return 0;
+}
