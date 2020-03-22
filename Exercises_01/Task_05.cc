7438bb78c86b2726de8886472fe214555d556924
Kacper Gorgoń <61627168+Kugger@users.noreply.github.com>
2020-03-22 11:03:36 +0100

    Ćwiczenia

---
+++ Exercises_01/Task_05.cc
@@ -0,0 +1,27 @@
+/**
+  * \author: Kacper Gorgoń
+  * \date: 2020.03.22
+  * \brief: Funkcja wyswietlajaca tyle * ile podamy n
+  * v0.01
+*/
+
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <ctype.h>
+using namespace std;
+
+int main() {
+	int n;
+
+	cout << "Podaj n: ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cout << '*';
+
+	cout << endl;
+
+	return 0;
+}
