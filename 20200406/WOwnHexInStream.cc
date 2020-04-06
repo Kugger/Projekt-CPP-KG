f99e60953b00f76caeffb7f3ffc0c77a7c9af36e
Kacper Gorgoń <61627168+Kugger@users.noreply.github.com>
2020-04-06 11:04:51 +0200

    Zajecia 06.04.2020

---
+++ 20200406/WOwnHexInStream.cc
@@ -0,0 +1,25 @@
+/**
+  * \author: Kacper Gorgon
+  * \date: 2020.04.06
+  * \brief: Dec to hex
+  * v0.01
+*/
+
+
+#include <iostream>
+#include <iomanip>
+#include <ios>
+using namespace std;
+
+// nie dziala
+
+int main() {
+
+	int x = 4;
+
+	//cout << "int: " << x << endl;
+	cout << x << " " << hex << x << " " << oct << x << endl;
+	cout << x << endl;
+
+	return 0;
+}
---
+++ 20200406/WOwnStrumienie.cc
@@ -0,0 +1,19 @@
+/**
+  * \author: Kacper Gorgon
+  * \date: 2020.04.06
+  * \brief: Sprawdzanie strumienia
+  * v0.01
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	float fT;
+	cin >> fT;
+	cout << fT << endl;
+
+	return 0;
+}
